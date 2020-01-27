fae61df0359fb320990523d7d8d4fb0440cbf483
James Giraldo <jamesgiiraldo@gmail.com>
2020-01-27 17:16:53 -0500

    rolify , devise  y simple form configurados

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module Seguimiento
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
+    end
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
