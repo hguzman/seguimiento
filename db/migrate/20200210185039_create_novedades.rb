2908b323a84a017dd1b1591ee5390fbccc61ac27
ojsilvera <ojsilvera@misena.edu.co>
2020-02-10 16:19:57 -0500

    Vistas show e index novedades

--- db/migrate/20200210185039_create_novedades.rb
+++ db/migrate/20200210185039_create_novedades.rb
@@ -1,6 +1,7 @@
 class CreateNovedades < ActiveRecord::Migration[6.0]
   def change
     create_table :novedades do |t|
+
       t.string :titulo
       t.text :descripcion
       t.references :user, foreign_key: true
