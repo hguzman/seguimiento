e069c86611171837c7512ad544fff6a40f017a9c
Miguel <zabalafmiguel@hotmail.com>
2020-02-17 12:18:25 -0500

    últimos cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'users/index'
+  end
   root to: 'home#index'
 
   devise_for :users
@@ -8,6 +11,8 @@ Rails.application.routes.draw do
   # Crea un CRUD para novedades, sólo index, y new, si necesitamos CREATE and PUT, omitiríamos el only
   # resources :novedades, only: [:index, :new]
   resources :novedades
+
   resources :users
+  resource :user, only: [:edit, :update]
 
 end
