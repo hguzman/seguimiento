56bd46e5059c8d3d604c52d665da0ae114550745
ojsilvera <ojsilvera@misena.edu.co>
2020-03-05 11:12:10 -0500

    issues#43

--- db/schema.rb
+++ db/schema.rb
@@ -23,14 +23,6 @@ ActiveRecord::Schema.define(version: 2020_03_05_152358) do
     t.index ["user_id"], name: "index_anotaciones_on_user_id"
   end
 
-  create_table "cursos", force: :cascade do |t|
-    t.integer "ficha"
-    t.string "nombre"
-    t.datetime "fecha"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "especialidades", force: :cascade do |t|
     t.string "nombre"
     t.datetime "created_at", precision: 6, null: false
