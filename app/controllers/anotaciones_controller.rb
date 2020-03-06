8c3b3414e5144a405f8993e6bfc71bb508a8e110
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-03-06 16:11:15 -0500

    He agregado la vista de crear nueva anotación

--- app/controllers/anotaciones_controller.rb
+++ app/controllers/anotaciones_controller.rb
@@ -1,4 +1,6 @@
 class AnotacionesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @anotaciones = current_user.anotaciones
   end
@@ -8,11 +10,21 @@ class AnotacionesController < ApplicationController
   end
 
   def new
-
+    @anotacion = Anotacion.new
+  end
+  
+  def create
+    @anotacion = Anotacion.new(anotacion_params)
+    if @anotacion.save
+      redirect_to users_path, notice: "Se ha creado la novedad"
+    else
+      render "new"
+    end
   end
 
-  def create
-
+  def anotacion_params
+    params.require(:anotacion).permit(:user_id, :descripcion)
   end
 
 end
+
