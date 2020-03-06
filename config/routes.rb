e1df2feef9cf670f08ed615ded42be74432d9f62
afleones <afleones@misena.edu.co>
2020-03-06 14:50:23 -0500

    Corregido el Bug de :Edit, :Update User

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,9 @@ Rails.application.routes.draw do
 
   get "toexcel", to: "fichas#toexcel"
 
-  resources :users, only: [:index, :ver_user, :show] do
+
+   resource :user, only: [:index, :edit, :destroy, :update , :show] do
      get :ver_user, on: :member
-   end
-   resource :user, only: [:edit, :destroy, :update , :show] do
      collection do
        patch 'update_password'
        get :change_password
