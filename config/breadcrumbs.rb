ebb3c28c1c7db4ac91876f67d86db2d019bbc772
ojsilvera <ojsilvera@misena.edu.co>
2020-03-20 15:59:09 -0500

    Corregir Breadcrumbs Ambientes

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 crumb :root do
-  link "Home", root_path
+  link 'Home', root_path
 end
 
 # breadcrumbs para especialidades
 
 crumb :especialidades do
-  link "Especialidades", especialidades_path
+  link 'Especialidades', especialidades_path
 end
 
 crumb :especialidad_new do
-  link "crear", new_especialidad_path
+  link 'crear', new_especialidad_path
   parent :especialidades
 end
 
@@ -17,7 +19,6 @@ crumb :especialidad_edit do |especialidad|
   link especialidad.nombre, especialidad
   parent :especialidades
 end
-
 
 crumb :especialidad_show do |especialidad|
   link especialidad.nombre, especialidad
@@ -27,11 +28,11 @@ end
 # breadcrumbs para fichas
 
 crumb :fichas do
-  link "fichas", fichas_path
+  link 'fichas', fichas_path
 end
 
 crumb :ficha_new do
-  link "crear", new_ficha_path
+  link 'crear', new_ficha_path
   parent :fichas
 end
 
@@ -39,7 +40,6 @@ crumb :ficha_edit do |ficha|
   link ficha.numero, ficha
   parent :fichas
 end
-
 
 crumb :ficha_show do |ficha|
   link ficha.numero, ficha
@@ -49,25 +49,30 @@ end
 # breadcrumbs para users
 
 crumb :users do
-  link "usuarios", users_path
+  link 'usuarios', users_path
 end
 
 # breadcrumbs para anotaciones
 
 crumb :anotaciones do
-  link "anotaciones", anotaciones_path
+  link 'anotaciones', anotaciones_path
 end
 
 crumb :anotacion_new do
-  link "crear", new_anotacion_path
+  link 'crear', new_anotacion_path
   parent :anotaciones
 end
 
 # breadcrumbs para devise
 
 crumb :devise_edit do |current_user|
-  link current_user.nombres,  edit_user_registration_path
+  link current_user.nombres, edit_user_registration_path
   parent :root
 end
 
+# breadcrumbs para ambientes
 
+crumb :ambientes do |_current_user|
+  link 'Ambientes', ambientes_path
+  parent :root
+end
