ceff318f10629598eb5fd614525cec4896c14735
Henry Guzmán <hguzman10@gmail.com>
2020-03-26 18:10:41 -0500

    Cambios en modelos

--- spec/models/ficha_spec.rb
+++ spec/models/ficha_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Ficha, type: :model do
   it { should have_db_column(:numero).of_type(:integer) }
-  it { should have_db_column(:naprendices).of_type(:integer) }
-  it { should have_db_column(:fechafin).of_type(:date) }
+  it { should have_db_column(:numero_aprendices).of_type(:integer) }
+  it { should have_db_column(:fecha_fin_at).of_type(:date) }
   it { should belong_to(:especialidad) }
   it { should have_many(:users) }
 end
