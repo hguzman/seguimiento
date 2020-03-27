5f1b5ddb3ca4cb2121d25f38ef39f296e8353dfa
ojsilvera <ojsilvera@misena.edu.co>
2020-03-27 08:43:00 -0500

    Ordenar los polices, aun no esta terminado

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
+# clase Police by user
 class UserPolicy
-    attr_reader :user, :users
-  
-    def initialize(user, users)
-      @user = user
-      @users = users
-    end
-  
-    def index?
-      @user.has_role? :admin
-    end
-   
+  attr_reader :user, :users
+
+  def initialize(user, users)
+    @user = user
+    @users = users
   end
+
+  def index?
+    @user.has_role? :admin
+  end
+end
