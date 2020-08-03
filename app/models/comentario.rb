c6baf1ec4a714e696c5480cf4fbd6529bcf416cf
ojsilvera <ojsilvera@misena.edu.co>
2020-08-03 10:40:21 -0500

    Validacion de campos en blanco formularios news

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,4 +1,5 @@
 class Comentario < ApplicationRecord
-	belongs_to :anotacion
-	record_operator_on :create, :update
+  validates :comentario, presence: true
+  belongs_to :anotacion
+  record_operator_on :create, :update
 end
--- app/models/ficha.rb
+++ app/models/ficha.rb
@@ -1,4 +1,9 @@
 class Ficha < ApplicationRecord
+    # Campos obligatorios
+    validates :numero, presence: true
+    validates :fecha_fin_at, presence: true
+    validates :numero_aprendices, presence: true
+
     belongs_to :programa
     has_many :users
 end
