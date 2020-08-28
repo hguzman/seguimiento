e88cac9a1922ee9d2d5e5b40ae615b6fe9505318
ojsilvera <ojsilvera@misena.edu.co>
2020-08-28 10:10:43 -0500

    Adicione otras extenciones para las eveidencias de comentarios y anotaciones, se ajusto el home de instructor y admin

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -39,7 +39,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(pdf)
+    %w(pdf jpg jpeg png)
   end
 
   # Override the filename of the uploaded files:
